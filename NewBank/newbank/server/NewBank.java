3744cd06a24da4c4bfca7f49b65da8e6cd8c81a7
katytaylor <katytaylor57@gmail.com>
2021-11-17 08:59:28 +0000

    Combine 3 methods into 1, passing account 'type' as parameter - reduce code duplication

--- NewBank/newbank/server/NewBank.java
+++ NewBank/newbank/server/NewBank.java
@@ -8,33 +8,33 @@ import java.util.HashMap;
  * also contains a validation method checkLogInDetails() that needs building on to validate passwords
  */
 public class NewBank {
-	
+
 	private static final NewBank bank = new NewBank();
 	private HashMap<String,Customer> customers;
-	
+
 	private NewBank() {
 		customers = new HashMap<>();
 		addTestData();
 	}
-	
+
 	private void addTestData() {
 		Customer bhagy = new Customer();
 		bhagy.addAccount(new Account("Main", 1000.0));
 		customers.put("Bhagy", bhagy);
-		
+
 		Customer christina = new Customer();
 		christina.addAccount(new Account("Savings", 1500.0));
 		customers.put("Christina", christina);
-		
+
 		Customer john = new Customer();
 		john.addAccount(new Account("Checking", 250.0));
 		customers.put("John", john);
 	}
-	
+
 	public static NewBank getBank() {
 		return bank;
 	}
-	
+
 	public synchronized CustomerID checkLogInDetails(String userName, String password) {
 		if(customers.containsKey(userName)) {
 			return new CustomerID(userName);
@@ -48,37 +48,23 @@ public class NewBank {
 			switch(request) {
 			//case "SHOWMYACCOUNTS" : return showMyAccounts(customer);
 				case "1" : return showMyAccounts(customer);
-				case "2" : return addMainAccount(customer);
-				case "3" : return addSavingsAccount(customer);
-				case "4" : return addCheckingAccount(customer);
+				case "2" : return addAccount(customer, "Main");
+				case "3" : return addAccount(customer, "Savings");
+				case "4" : return addAccount(customer, "Checking");
 			default : return "FAIL";
 			}
 		}
 		return "FAIL";
 	}
-	
+
 	private String showMyAccounts(CustomerID customer) {
 		return (customers.get(customer.getKey())).accountsToString();
 	}
 
-	private String addMainAccount(CustomerID customer) {
+	private String addAccount(CustomerID customer, String type) {
 		Customer userName = customers.get(customer.getKey());
-		Account main = new Account("Main", 0.00);
-		userName.addAccount(main);
-		return ("SUCCESS New account " + main.toString());
-	}
-
-	private String addSavingsAccount(CustomerID customer) {
-		Customer userName = customers.get(customer.getKey());
-		Account savings = new Account("Savings", 0.00);
-		userName.addAccount(savings);
-		return ("SUCCESS New account " + savings.toString());
-	}
-
-	private String addCheckingAccount(CustomerID customer) {
-		Customer userName = customers.get(customer.getKey());
-		Account checking = new Account("Checking", 0.00);
-		userName.addAccount(checking);
-		return ("SUCCESS New account " + checking.toString());
+		Account account = new Account(type, 0.00);
+		userName.addAccount(account);
+		return ("SUCCESS New account " + account.toString());
 	}
 }
